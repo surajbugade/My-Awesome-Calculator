41b10cb697a3d5051071eb08a3e1e9fed9644f5f
Suraj Bugade <surajbugade10@gmail.com>
2021-04-26 00:22:35 +0530

    Styled Calculator & changed some buttons.

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   let operators = new Set("+-*/%.");
   
   let buttons = [
-    "(",
-    ")",
-    "%",
+    "^_^",
+    "AC",
     "CE",
+    "%",
     "7",
     "8",
     "9",
@@ -33,26 +33,37 @@ function App() {
   ];
 
   let evaluateExpression = function () {
-    // eslint-disable-next-line
-    let evaluation = eval(expression);
-    setOldExpression(expression + "=");
-    expression = setExpression(String(evaluation));
-    setPrev("ANS");
+    try {
+        // eslint-disable-next-line
+      let evaluation = eval(expression);
+      setOldExpression(expression + " =");
+      expression = setExpression(String(evaluation));
+      setPrev("ANS");
+    } catch {
+      setOldExpression(expression + " =");
+      setExpression("Error");
+      setPrev("Err");
+    }
   }
 
   let putNumerics = function (value) {
-    if(prev == "ANS") {
+    if(prev === "ANS") {
       setOldExpression("ANS = " + expression);
       setExpression(value);
-    }
-    else {
+    } else if(prev === "AC") {
+      setExpression(expression.slice(0, -1) + value);
+    } else if(prev === "Err") {
+      setExpression(value);
+    } else {
       setExpression(expression + value);
     }
     setPrev("NUM");
   }
 
   let putOperator = function (value) {
-    if(prev != "OP"){
+    if(prev === "Err") {
+      setExpression("0" + value);
+    } else if(prev !== "OP"){
       setExpression(expression + value);
     } else {
       setExpression(expression.slice(0, -1) + value);
@@ -66,11 +77,23 @@ function App() {
     }
     setPrev("DEL");
   }
+  
+  let clearAll = function () {
+    if(expression.length >= 1){
+      if(prev === "ANS") {
+        setOldExpression("ANS = " + expression);
+      }
+      setExpression("0");
+    }
+    setPrev("AC");
+  }
 
   let handleKeyUp = function (event) {
     console.log(event.key);
     if (event.key === "Backspace") {
       putDelete();
+    } else if (event.key === "Delete") {
+      clearAll();
     } else if (numerics.has(event.key)) {
       putNumerics(event.key);
     } else if(operators.has(event.key)) {
@@ -84,18 +107,32 @@ function App() {
     <div className="App" tabIndex={0} onKeyUp={handleKeyUp}>
       <div style = {{
         padding: "10px",
-        borderRadius: "10px",
-        background: "#444444",
+        borderRadius: "15px",
+        background: "#333333",
         display: "flex",
-        flexDirection: "column",
+        flexDirection: "column"
       }}>
-        <h1 style = {{
-          color: "#ffffff",
-          textAlign: "center"
-        }}>
-          My Awesome Calculator
-        </h1>
-      
+        <div
+        style = {{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+        > 
+          <img src = "calculate.png" alt = "Calc"
+            style = {{
+              height: "50px",
+              marginRight: "10px"
+            }} />
+          <h1 style = {{
+            color: "#ffffff",
+            textAlign: "center",
+            fontFamily: "Lobster Two, cursive",
+            fontSize: "38px"
+          }}>
+            My Awesome Calculator
+          </h1>
+        </div>
       <div
         style={{
           width: "400px",
@@ -107,10 +144,10 @@ function App() {
           padding: "20px",
           borderRadius: "10px",
           overflow: "hidden",
-          margin: "20px",
+          margin: "20px"
         }}
       >
-        <h5>{oldExpression}</h5>
+        <h4>{oldExpression}</h4>
         <h1>{expression}</h1>
       </div>
 
@@ -130,17 +167,21 @@ function App() {
       >
         {buttons.map(function (buttonValue, idx) {
           return (
-            <button
+            <button className = "Calc" 
               style={{
                 width: "90px",
                 height: "40px",
                 padding: "5px",
                 margin: "5px",
-                fontSize: "19px",
+                borderRadius: "10px",
+                fontSize: "20px",
+                fontFamily: "JetBrains Mono, monospace"
               }}
               onClick={function () {
                 if (buttonValue === "CE") {
                   putDelete();
+                } else if (buttonValue === "AC") {
+                  clearAll();
                 } else if (numerics.has(buttonValue)) {
                   putNumerics(buttonValue);
                 } else if(operators.has(buttonValue)) {
@@ -149,7 +190,7 @@ function App() {
                   evaluateExpression();
                 }
               }}
-            >
+              >
               {buttonValue}
             </button>
           );
